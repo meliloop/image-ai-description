73818076285c6c9c8dfb4e272fcffd60c6269c39
Meli <meli@bracketmedia.com>
2024-06-26 16:16:08 -0300

    limpio barras

--- api/app.py
+++ api/app.py
@@ -13,11 +13,12 @@ def verify_token(token):
     return token == hashlib.md5('bracket-ai-services'.encode()).hexdigest()
 
 def get_image_data(image_url):
-    response = requests.get(image_url)
-    if response.status_code != 200:
-        return {"error": "Could not download the image"}
-
     try:
+        image_url = image_url.replace('\\/', '/')
+        response = requests.get(image_url)
+        if response.status_code != 200:
+            return {"error": "Could not download the image"}
+        
         prompt = (
             f"You are an expert SEO analyst, we need to generate metadata for this image, following google's criteria for best performance please generate:\n"
             f"Caption: Provide a caption for the image.\n"
@@ -54,7 +55,7 @@ def get_image_data(image_url):
         #tags = re.search(r'Tags: (.+)', output).group(1).split(", ") if re.search(r'Tags: (.+)', output) else ''
         description = re.search(r'Description: (.+)', output).group(1) if re.search(r'Description: (.+)', output) else ''
         alt_text = re.search(r'Alt Text: (.+)', output).group(1) if re.search(r'Alt Text: (.+)', output) else ''
-            
+
         result = {
             "caption": caption,
             #"tags": tags,
